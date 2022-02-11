aae5c10ccc7a44a47d3db7d862bb0661d985e065
Jaiswar <jaiswarnilesh2002@gmail.com>
2022-02-11 15:14:03 +0530

    updated

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export {default as Layout} from './Layout'
-export {default as Login} from './Login/login'
+export {default as Login} from './Login/Login'
 export {default as Navbar} from './Navbar/Navbar'
