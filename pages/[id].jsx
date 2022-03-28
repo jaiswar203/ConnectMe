940d57622acab2f0ba351fd8ed0023ae7981e304
Jaiswar <jaiswarnilesh2002@gmail.com>
2022-03-28 21:43:32 +0530

    updated

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router"
 import { useEffect } from "react"
+import {v4 as uuid} from 'uuid'
 import { useSelector } from "react-redux"
 import { User } from "../src/components"
 
@@ -9,9 +10,15 @@ const Detail = () => {
   const state = useSelector((state) => state.AuthRedu)
   const authData = state?.authData
   
+  const uniqueIdForVisitor=uuid()
 
+  
   useEffect(()=>{
+    const is_cookie_exist=localStorage.getItem("unique")
 
+    if(is_cookie_exist===null){
+      localStorage.setItem("unique",uniqueIdForVisitor)
+    }
   },[authData])
   return (
     <User edit={false} />
