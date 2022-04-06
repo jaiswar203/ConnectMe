19dc43f60f3fa492881eec2b06a49b083b0924d6
Jaiswar <jaiswarnilesh2002@gmail.com>
2022-04-06 20:38:20 +0530

    updated

--- src/components/Footer/SearchBar.jsx
+++ src/components/Footer/SearchBar.jsx
@@ -26,19 +26,19 @@ const SearchBar = ({setSearchBar}) => {
     }
     
     return (
-        <div className="connectme__search">
-            <div class="Card">
-                <div className="close" onClick={()=>setSearchBar(false)}>
+        <div classNameName="connectme__search">
+            <div className="Card">
+                <div classNameName="close" onClick={()=>setSearchBar(false)}>
                     <GiCancel />
                 </div>
-                <div class="CardInner">
+                <div className="CardInner">
                     <label>Search For Profile</label>
-                    <div class="container">
-                        <div class="InputContainer">
+                    <div className="container">
+                        <div className="InputContainer">
                             <input placeholder="Username"  onChange={(e)=>setsearchText(e.target.value)} onKeyPress={(e)=>{e.key==="Enter" && pushToSearch()}} />
                         </div>
-                        <div class="Icon" onClick={pushToSearch}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#657789" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8" /><line x1="21" y1="21" x2="16.65" y2="16.65" /></svg>
+                        <div className="Icon" onClick={pushToSearch}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#657789" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="feather feather-search"><circle cx="11" cy="11" r="8" /><line x1="21" y1="21" x2="16.65" y2="16.65" /></svg>
                         </div>
                     </div>
                 </div>
