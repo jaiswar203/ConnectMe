3811d922d438ece1d29e7a88a8a64195ce017e3a
Jaiswar <jaiswarnilesh2002@gmail.com>
2022-04-26 16:14:31 +0530

    updated

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,6 +6,13 @@ import { FaTimes } from 'react-icons/fa'
 import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
+import { ToastContainer, toast } from 'react-toast'
+import Image from "next/image";
+import Illus from "../../../illustrations/illus.jsx"
+// import Illus1 from "../../../illustrations/illus1.jsx"
+import Illus1 from "../../../illustrations/girl.jsx"
+import Wave from "../../../illustrations/wave.jsx"
+
 
 import Gmail from "../User/logo/gmail";
 
@@ -97,164 +104,176 @@ const Login = () => {
     } else if (authData && SignUp) {
       router.push(`/confirm/${authData?.existingUser?.username}?confirmyourmail=true`)
     }
+
+
   }, [dispatch, authData, SignUp, router])
 
 
-
-  console.log({errors})
   return (
-    <div className='connectme__login'>
-      <div className="connectme__login-intro">
-        <motion.h1 initial={{ x: 100, opacity: 0 }} whileInView={{ x: 0, opacity: 1 }} transition={{ type: "spring", stiffness: 200 }}>{SignUp ? "Create" : "Login to "} Your Account</motion.h1>
-        <motion.p initial={{ x: 100, opacity: 0 }} whileInView={{ x: 0, opacity: 1 }} transition={{ type: "spring", stiffness: 200, delay: .8 }}>Create Awesome Intro for your film debut</motion.p>
-      </div>
-      <div className="connectme__login-content">
-        <motion.form className="connectme__login-content__manual" initial={{ y: 100, opacity: 0 }} whileInView={{ y: 0, opacity: 1 }} transition={{ type: "spring", stiffness: 200, duration: 1 }} onSubmit={handleSubmit(onSubmit)}>
-          {SignUp && (
-            <>
-              <TextField variant="outlined" style={{ marginRight: "5px",marginBottom:"1rem" }} label="UserName" focused={errors?.username && true} type="text" fullWidth color={errors?.username || state?.error?.data ? "secondary" : "primary"} {...register("username", { required: true })} />
-              {
-                errors?.username?.type==="required" && (
-                  <span style={{marginTop:"-1rem"}}>Please Enter your Username</span>
-                )
-              }
-              {state?.error?.data?.message === "Username Already Exist" && (
-                <span style={{marginTop:"-1rem"}}> {"UserName Already Taken"}  </span>
-              )}
-              <div className="name">
-                <div className="name__firstname">
-                  <TextField variant="outlined" style={{ marginRight: "5px" }} label="First Name" focused={errors?.name && true} type="text" fullWidth color={errors?.name ? "secondary" : "primary"} {...register("name", { required: true })} />
-                  <span>{errors?.name?.type==="required" && "Please Enter Your FirstName"}</span>
-                </div>
-                <div className="name__lastname">
-                  <TextField variant="outlined" label="Last Name" focused={errors?.name && true} type="text" fullWidth color={errors?.name ? "secondary" : "primary"} {...register("lname", { required: true })} />
-                  <span>{errors?.lname?.type==="required" && "Please Enter Your LastName"}</span>
-                </div>
-              </div>
-            </>
-          )}
-          <div className="email">
-            <TextField variant="outlined" label="Email" focused={errors?.email && true} type="email" fullWidth color={errors?.email && SignUp ? "secondary" : errors?.email?.type==="required" && !SignUp ? "secondary" : "primary"} {...register("email", { required: true, pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ })} />
-            {SignUp ? (
-              <span>{errors?.email?.type === "required" && "Please Enter Your Email"}</span>
-              ) : (
-                <span>{errors?.email?.type === "required" && "Please Enter Your Email"}</span>
-            )}
-            {
-              errors?.email?.type === "required" ? null : state?.error?.data?.message === "User Already Exist" && (
-                <span>User Already Exist</span>
-              )
-            }
+    <>
+      <ToastContainer position="bottom-right" />
+      <div className='connectme__login'>
+        <div className="left">
+          <Wave />
 
-            {
-              !SignUp ?
-                state?.error?.status === 404 && (
-                  <span>No User Founded</span>
-                ) : null
-            }
+          <div className="left__center">
+            <Illus1 />
           </div>
-          <div className="password">
-            <TextField variant="outlined" label="Password" focused={errors?.password} type={icon ? "password" : "text"} color={errors?.password && SignUp ? "secondary" : errors?.password?.type==="required" && !SignUp ? "secondary" :"primary"} {...register("password", { required: true, minLength: SignUp && 8 })} fullWidth InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={() => setIcon(!icon)}>
-                    {icon ? <AiFillEye /> : <AiFillEyeInvisible />}
-                  </IconButton>
-                </InputAdornment>
-              )
-            }} />
-            {SignUp ? (
-              <>
-                <span>{errors?.password?.type === "required" && "Please Enter Your Password"}</span>
-                <span>{errors?.password?.type === "minLength" && "PassWord should be greater than 8 digit"}</span>
-              </>
-            ): (
-              <span>{errors?.password?.type === "required" && "Please Enter Your Password"}</span>
-            ) }
-            {
-              state?.error?.status === 400 && (
-                <span>Wrong Password or username</span>
-              )
-            }
-          </div>
-          <motion.button type="submit" className="submit">
-            {
-              state.isLoading ? (
-                <ClipLoader color="#000" size={25} />
-              ) : (
-                <h2>Submit</h2>
-              )
-            }
-          </motion.button>
-        </motion.form>
-        <div className="divider">
-          <span>
-            <AiOutlineMinus />
-          </span>
-          <h2>
-            OR
-          </h2>
-          <span>
-            <AiOutlineMinus />
-          </span>
         </div>
-        <div className="auth">
-          {SignUp ? (
-            <motion.div className="google" initial={{ x: 200, opacity: 0, border: "1px solid black" }} animate={{ x: 0, opacity: 1 }} whileHover={{ x: 5, y: -5, border: "1px solid black" }} onClick={() => setAskUserNamePermission(!askUserNamePermission)} >
-              <Gmail w={33} h={33} />
-              <p>{SignUp ? "SignUp" : "SignIn"} With Google</p>
-            </motion.div>
+        <div className="right">
+          <div className="connectme__login-intro">
+            <motion.h1 initial={{ y: 100, opacity: 0 }} whileInView={{ y: 0, opacity: 1 }} transition={{ type: "spring", stiffness: 200 }}>{SignUp ? "Create" : "Login to "} Your Account</motion.h1>
+            <motion.p initial={{ y: 100, opacity: 0 }} whileInView={{ y: 0, opacity: 1 }} transition={{ type: "spring", stiffness: 200, delay: .8 }}>Create Awesome Intro for your film debut</motion.p>
+          </div>
+          <div className="connectme__login-content">
+            <motion.form className="connectme__login-content__manual" initial={{ y: 100, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ type: "spring", stiffness: 200, duration: 1 }} onSubmit={handleSubmit(onSubmit)}>
+              {SignUp && (
+                <>
+                  <TextField variant="outlined" style={{ marginRight: "5px", marginBottom: "1rem" }} label="UserName" focused={errors?.username && true} type="text" fullWidth color={errors?.username || state?.error?.data ? "secondary" : "primary"} {...register("username", { required: true })} />
+                  {
+                    errors?.username?.type === "required" && (
+                      <span style={{ marginTop: "-1rem" }}>Please Enter your Username</span>
+                    )
+                  }
+                  {state?.error?.data?.message === "Username Already Exist" && (
+                    <span style={{ marginTop: "-1rem" }}> {"UserName Already Taken"}  </span>
+                  )}
+                  <div className="name">
+                    <div className="name__firstname">
+                      <TextField variant="outlined" style={{ marginRight: "5px" }} label="First Name" focused={errors?.name && true} type="text" fullWidth color={errors?.name ? "secondary" : "primary"} {...register("name", { required: true })} />
+                      <span>{errors?.name?.type === "required" && "Please Enter Your FirstName"}</span>
+                    </div>
+                    <div className="name__lastname">
+                      <TextField variant="outlined" label="Last Name" focused={errors?.name && true} type="text" fullWidth color={errors?.name ? "secondary" : "primary"} {...register("lname", { required: true })} />
+                      <span>{errors?.lname?.type === "required" && "Please Enter Your LastName"}</span>
+                    </div>
+                  </div>
+                </>
+              )}
+              <div className="email">
+                <TextField variant="outlined" label="Email" focused={errors?.email && true} type="email" fullWidth color={errors?.email && SignUp ? "secondary" : errors?.email?.type === "required" && !SignUp ? "secondary" : "primary"} {...register("email", { required: true, pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ })} />
+                {SignUp ? (
+                  <span>{errors?.email?.type === "required" && "Please Enter Your Email"}</span>
+                ) : (
+                  <span>{errors?.email?.type === "required" && "Please Enter Your Email"}</span>
+                )}
+                {
+                  errors?.email?.type === "required" ? null : state?.error?.data?.message === "User Already Exist" && (
+                    <span>User Already Exist</span>
+                  )
+                }
 
-          ) : (
-            <GoogleLogin
-              clientId="997233871583-5eppubk9g40htsamhqs4t15mcer7ejk0.apps.googleusercontent.com"
-              render={(renderprops) => (
-                <motion.div className="google" initial={{ x: 200, opacity: 0, border: "1px solid black" }} animate={{ x: 0, opacity: 1 }} whileHover={{ x: 5, y: -5, background: "black", color: "#3080C0", border: "1px solid #3080C0" }} onClick={renderprops.onClick} >
+                {
+                  !SignUp ?
+                    state?.error?.status === 404 && (
+                      <span>No User Founded</span>
+                    ) : null
+                }
+              </div>
+              <div className="password">
+                <TextField variant="outlined" label="Password" focused={errors?.password} type={icon ? "password" : "text"} color={errors?.password && SignUp ? "secondary" : errors?.password?.type === "required" && !SignUp ? "secondary" : "primary"} {...register("password", { required: true, minLength: SignUp && 8 })} fullWidth InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton onClick={() => setIcon(!icon)}>
+                        {icon ? <AiFillEye /> : <AiFillEyeInvisible />}
+                      </IconButton>
+                    </InputAdornment>
+                  )
+                }} />
+                {SignUp ? (
+                  <>
+                    <span>{errors?.password?.type === "required" && "Please Enter Your Password"}</span>
+                    <span>{errors?.password?.type === "minLength" && "PassWord should be greater than 8 digit"}</span>
+                  </>
+                ) : (
+                  <span>{errors?.password?.type === "required" && "Please Enter Your Password"}</span>
+                )}
+                {
+                  state?.error?.status === 400 && (
+                    <span>Wrong Password or username</span>
+                  )
+                }
+              </div>
+              <motion.button type="submit" className="submit">
+                {
+                  state.isLoading ? (
+                    <ClipLoader color="#000" size={25} />
+                  ) : (
+                    <h2>Submit</h2>
+                  )
+                }
+              </motion.button>
+            </motion.form>
+            <div className="divider">
+              <span>
+                <AiOutlineMinus />
+              </span>
+              <h2>
+                OR
+              </h2>
+              <span>
+                <AiOutlineMinus />
+              </span>
+            </div>
+            <div className="auth">
+              {SignUp ? (
+                <motion.div className="google" initial={{ x: 200, opacity: 0, border: "1px solid black" }} animate={{ x: 0, opacity: 1 }} whileHover={{ scale: 1.03, border: "1px solid black" }} onClick={() => setAskUserNamePermission(!askUserNamePermission)} >
                   <Gmail w={33} h={33} />
                   <p>{SignUp ? "SignUp" : "SignIn"} With Google</p>
                 </motion.div>
-              )}
-              onRequest={() => { console.log("Request Maded") }}
-              onSuccess={googleSuccess}
-              onFailure={googleFailure}
-              cookiePolicy="single_host_origin" />
 
-          )}
-
-        </div>
-      </div>
-      {
-        SignUp && (
-
-          askUserNamePermission && (
-
-            <div className="connectme__login-model">
-              <div className="cross" onClick={() => setAskUserNamePermission(!askUserNamePermission)}>
-                <FaTimes />
-              </div>
-              <motion.div className="connectme__login-model__data" initial={{ y: 200, opacity: 0 }} animate={{ y: 0, opacity: 1 }}>
-                <TextField variant="outlined" label="UserName" color={state?.error?.status === 404 ? "error" : "primary"} type="text" className="username" onChange={(e) => setaskUserName(e.target.value)} fullWidth />
-                {state?.error?.status === 404 && !SignUp && askUserNamePermission ? (
-                  <span>
-                    User Already Exist  
-                  </span>
-                ) : null}
+              ) : (
                 <GoogleLogin
                   clientId="997233871583-5eppubk9g40htsamhqs4t15mcer7ejk0.apps.googleusercontent.com"
                   render={(renderprops) => (
-                    <motion.button className="google" disabled={askUserName === "" ? true : false} style={{ background: askUserName === '' ? "gray" : "white" }} whileHover={{ background: "#3080C0", color: "white" }} initial={{ x: 200, opacity: 0, border: "1px solid black" }} animate={{ x: 0, opacity: 1 }} whileTap={{ scale: 1.02 }} onClick={renderprops.onClick} >
-                      <p>SignUp With Google</p>
-                    </motion.button>
+                    <motion.div className="google" initial={{ x: 200, opacity: 0, border: "1px solid black" }} animate={{ x: 0, opacity: 1 }} whileHover={{ scale: 1.03, background: "black", color: "#3080C0", border: "1px solid #3080C0" }} onClick={renderprops.onClick} >
+                      <Gmail w={33} h={33} />
+                      <p>{SignUp ? "SignUp" : "SignIn"} With Google</p>
+                    </motion.div>
                   )}
+                  onRequest={() => { console.log("Request Maded") }}
                   onSuccess={googleSuccess}
                   onFailure={googleFailure}
                   cookiePolicy="single_host_origin" />
-              </motion.div>
+
+              )}
+
             </div>
-          )
-        )
-      }
-    </div>
+          </div>
+          {
+            SignUp && (
+
+              askUserNamePermission && (
+
+                <div className="connectme__login-model">
+                  <div className="cross" onClick={() => setAskUserNamePermission(!askUserNamePermission)}>
+                    <FaTimes />
+                  </div>
+                  <motion.div className="connectme__login-model__data" initial={{ y: 200, opacity: 0 }} animate={{ y: 0, opacity: 1 }}>
+                    <TextField variant="outlined" label="UserName" color={state?.error?.status === 404 ? "error" : "primary"} type="text" className="username" onChange={(e) => setaskUserName(e.target.value)} fullWidth />
+                    {state?.error?.status === 404 && !SignUp && askUserNamePermission ? (
+                      <span>
+                        User Already Exist
+                      </span>
+                    ) : null}
+                    <GoogleLogin
+                      clientId="997233871583-5eppubk9g40htsamhqs4t15mcer7ejk0.apps.googleusercontent.com"
+                      render={(renderprops) => (
+                        <motion.button className="google" disabled={askUserName === "" ? true : false} style={{ background: askUserName === '' ? "gray" : "white" }} whileHover={{ background: "#3080C0", color: "white" }} initial={{ x: 200, opacity: 0, border: "1px solid black" }} animate={{ x: 0, opacity: 1 }} whileTap={{ scale: 1.02 }} onClick={renderprops.onClick} >
+                          <p>SignUp With Google</p>
+                        </motion.button>
+                      )}
+                      onSuccess={googleSuccess}
+                      onFailure={googleFailure}
+                      cookiePolicy="single_host_origin" />
+                  </motion.div>
+                </div>
+              )
+            )
+          }
+        </div>
+      </div>
+    </>
   );
 };
 
