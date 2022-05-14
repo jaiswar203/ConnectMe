567fd63a6ebce10e389c63a031d0c0c3ec85aabd
Jaiswar <jaiswarnilesh2002@gmail.com>
2022-05-14 20:33:16 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const devServer="http://localhost:4000"
 const prodServer="https://connecmev1.herokuapp.com"
 
-const API = axios.create({ baseURL: devServer });
+const API = axios.create({ baseURL: prodServer });
 
 
 API.interceptors.request.use((req) => {
